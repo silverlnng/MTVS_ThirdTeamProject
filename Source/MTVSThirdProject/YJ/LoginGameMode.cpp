94485a65c1244b8232edb021ae142852235cd6fd
silverlnng <112385982+silverlnng@users.noreply.github.com>
2024-09-12 16:56:02 +0900

    rabbit 다시 오토리깅

--- Source/MTVSThirdProject/YJ/LoginGameMode.cpp
+++ Source/MTVSThirdProject/YJ/LoginGameMode.cpp
@@ -2,19 +2,51 @@
 
 
 #include "LoginGameMode.h"
-
+#include "LoginFirstWidget.h"
+#include "OnlineSubsystem.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/EditableText.h"
 
 void ALoginGameMode::BeginPlay()
 {
 	Super::BeginPlay();
 	if(loginWidget != nullptr)
 	{
-		UUserWidget* loginUI = CreateWidget<UUserWidget>(GetWorld(),loginWidget);
+		loginUI = CreateWidget<ULoginFirstWidget>(GetWorld(),loginWidget);
 		if(loginUI != nullptr)
 		{
 			loginUI->AddToViewport();
 			GetWorld()->GetFirstPlayerController()->SetShowMouseCursor(true);
+			
+			// 스팀서버 접속이라면
+
+			FTimerHandle handle;
+			GetWorldTimerManager().SetTimer(handle,[this]()
+			{
+				if(IOnlineSubsystem::Get()->GetSubsystemName()=="STEAM")
+				{
+					const ULocalPlayer* LocalPlayer = GetWorld()->GetFirstLocalPlayerFromController();
+					FString name = *LocalPlayer->GetPreferredUniqueNetId().GetUniqueNetId()->ToString();
+					GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red,*name);
+					//loginUI->eText_hostName->SetText(FText::FromString(name));
+					loginUI->eText_hostName->SetHintText(FText::FromString(name));
+						//
+				}
+				else
+				{
+					GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red,"No such login");
+					const ULocalPlayer* LocalPlayer = GetWorld()->GetFirstLocalPlayerFromController();
+					FString name = *LocalPlayer->GetPreferredUniqueNetId().GetUniqueNetId()->ToString();
+					GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red,*name);
+					//loginUI->eText_hostName->SetText(FText::FromString(name));
+					loginUI->eText_hostName->SetHintText(FText::FromString(name));
+				}
+			},3.f,false);
+			
+			
+			
 		}
 	}
+	
+	
 }
