b1db96c65ea925d5e3cdbedad487c5b3f8676985
jinheyong <soogo880@gmail.com>
2024-09-23 10:18:18 +0900

    Commit 각 WidgetC++클래스 생성
    각 WidgetC++클래스 생성

---
+++ Source/MTVSThirdProject/Public/AJH_InformationWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AJH_InformationWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MTVSTHIRDPROJECT_API UAJH_InformationWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
