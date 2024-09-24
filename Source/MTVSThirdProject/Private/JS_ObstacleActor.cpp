83591639cb5414c1e7ee2aac67dad00166a6f965
jeongsoll <jeongsol131@gmail.com>
2024-09-24 17:00:09 +0900

    트리 데미지 넣는 부분 임시 수정

--- Source/MTVSThirdProject/Private/JS_ObstacleActor.cpp
+++ Source/MTVSThirdProject/Private/JS_ObstacleActor.cpp
@@ -59,8 +59,7 @@ void AJS_ObstacleActor::Tick(float DeltaTime)
 
 void AJS_ObstacleActor::GetDamage_Implementation(bool damage)
 {
-	treeCurHP--;
-	//Server_GetDamage(damage);
+	Server_GetDamage(damage);
 }
 
 void AJS_ObstacleActor::SetCurHP_Implementation(float amount)
